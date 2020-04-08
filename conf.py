71fb3811d042a0d139cd2846c9f466885871015d
ZigZagK <36411613+ZigZagK@users.noreply.github.com>
2020-04-08 11:54:28 +0800

    Update conf.py

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 """
 
 # For Maverick
-site_prefix = "/Blog-With-GitHub-Boilerplate/"
+site_prefix = "/Wiki/"
 source_dir = "../src/"
 build_dir = "../dist/"
 index_page_size = 10
@@ -19,14 +19,14 @@ enable_jsdelivr = {
 }
 
 # 站点设置
-site_name = "test"
-site_logo = "${static_prefix}logo.png"
-site_build_date = "2019-12-18T16:51+08:00"
-author = "熊猫小A"
-email = "hi@imalan.cn"
-author_homepage = "https://www.imalan.cn"
-description = "只坚持一种正义。我的正义。"
-key_words = ['Maverick', '熊猫小A', 'Galileo', 'blog']
+site_name = "ZigZagK's Wiki"
+site_logo = "${static_prefix}ZigZagK.png"
+site_build_date = "2020-4-8T11:51+08:00"
+author = "ZigZagK"
+email = "zigzagk@qq.com"
+author_homepage = "https://zigzagk.top"
+description = "Never give up fighting!"
+key_words = ['Maverick', 'ZigZagK', 'Kepler', 'wiki']
 language = 'zh-CN'
 external_links = [
     {
@@ -35,9 +35,9 @@ external_links = [
         "brief": "🏄‍ Go My Own Way."
     },
     {
-        "name": "三無計劃",
-        "url": "https://www.imalan.cn",
-        "brief": "熊猫小A的主页。"
+        "name": "ZigZagK的博客",
+        "url": "https://zigzagk.top",
+        "brief": "Never give up fighting!"
     }
 ]
 nav = [
@@ -60,19 +60,9 @@ nav = [
 
 social_links = [
     {
-        "name": "Twitter",
-        "url": "https://twitter.com/AlanDecode",
-        "icon": "gi gi-twitter"
-    },
-    {
         "name": "GitHub",
-        "url": "https://github.com/AlanDecode",
+        "url": "https://github.com/ZigZagK",
         "icon": "gi gi-github"
-    },
-    {
-        "name": "Weibo",
-        "url": "https://weibo.com/5245109677/",
-        "icon": "gi gi-weibo"
     }
 ]
 
